7bdc073e9e498a158afbe1372c70c68cac7e83eb
rtkay123 <rodney.kanjala@sybrin.com>
2023-12-01 12:33:26 +0200

    stream to elastic only

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -28,6 +28,6 @@ const streams = [
   }
 ]
 
-//export const logger = pino({ level: 'trace', ...ecsOpts }, streamToElastic)
-export const logger = pino(pino.multistream(streams))
+export const logger = pino({ level: 'trace', ...ecsOpts }, stream)
+//export const logger = pino(pino.multistream(streams))
 //export const logger = pino({ level: 'info' }, process.stdout)
