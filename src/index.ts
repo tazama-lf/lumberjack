4efcddda855a8b6edae6e104f675554e1f6c1e0e
rtkay123 <rodney.kanjala@sybrin.com>
2023-12-11 12:34:33 +0200

    chore: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import 'dotenv/config'
-import { natsConnection } from "./services/nats.js"
-import { logger } from "./services/logger.js";
+import 'dotenv/config';
+import { natsConnection } from './services/nats.js';
+import { logger } from './services/logger.js';
 import { subject } from './config/server.js';
-import { decodeLogBuffer } from '@frmscoe/frms-coe-lib/lib/helpers/protobuf.js'
+import { decodeLogBuffer } from '@frmscoe/frms-coe-lib/lib/helpers/protobuf.js';
 
 (async () => {
   const nats = await natsConnection;
@@ -12,24 +12,26 @@ import { decodeLogBuffer } from '@frmscoe/frms-coe-lib/lib/helpers/protobuf.js'
 
   for await (const m of subscription) {
     const logMessage = decodeLogBuffer(m.data as Buffer);
-    if (logMessage) {
+    if (logMessage != null) {
       const { level } = logMessage;
       switch (level) {
-        case "fatal":
-          logger.fatal(logMessage)
+        case 'fatal':
+          logger.fatal(logMessage);
           break;
-        case "trace":
-          logger.trace(logMessage)
+        case 'trace':
+          logger.trace(logMessage);
           break;
-        case "debug":
-          logger.debug(logMessage)
+        case 'debug':
+          logger.debug(logMessage);
           break;
-        case "error":
-          logger.error(logMessage)
+        case 'error':
+          logger.error(logMessage);
           break;
         default:
-          logger.info(logMessage)
+          logger.info(logMessage);
       }
     }
   }
-})();
+})().catch((e) => {
+  console.error(e);
+});
