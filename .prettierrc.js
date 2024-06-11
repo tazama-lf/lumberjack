a5d3453fb9a5421898fc58910a8791138a63563e
rtkay123 <rodney.kanjala@sybrin.com>
2024-06-11 15:02:34 +0200

    build: update dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module.exports = {
-  "printWidth": 80,
-  "tabWidth": 2,
-  "useTabs": false,
-  "semi": true,
-  "singleQuote": true,
-  "quoteProps": "as-needed",
-  "trailingComma": "all",
-  "bracketSpacing": true,
-  "arrowParens": "always",
-  "endOfLine": "lf"
-}
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
