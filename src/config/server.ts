b62dc02389e85cba45b3b36d39fb7ac7fac200ad
rtkay123 <rodney.kanjala@sybrin.com>
2024-10-15 16:38:16 +0200

    build: update env

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
-import { validateEnvVar } from '@tazama-lf/frms-coe-lib/lib/helpers/env';
 import { type LogConfig, validateLogConfig } from '@tazama-lf/frms-coe-lib/lib/config/monitoring.config';
+import { validateEnvVar } from '@tazama-lf/frms-coe-lib/lib/config';
 
 export const stdOut = validateEnvVar('STDOUT', 'boolean', true) || false;
 
